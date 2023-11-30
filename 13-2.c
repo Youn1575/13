d656fe71a9f6eb05f754d07da9d07d6eeeb8177f
youn1575 <youn461515@gmail.com>
2023-11-30 15:32:07 +0900

    13-2

---
+++ 13-2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// 구조체 형식 선언
+struct point {
+    int x;
+    int y;
+};
+
+int main(int argc, char *argv[]) {
+
+    // instance 선언
+    struct point p1, p2;
+    int xdiff, ydiff;
+    double dist;
+
+    printf("input p1 coordinate (x y) : ");
+    scanf("%d %d", &p1.x, &p1.y);
+
+    printf("input p2 coordinate (x y) : ");
+    scanf("%d %d", &p2.x, &p2.y);
+
+    // distance
+    xdiff = p2.x - p1.x;
+    ydiff = p2.y - p1.y;
+
+    dist = sqrt(xdiff * xdiff + ydiff * ydiff);
+
+    printf("distance: %f\n", dist);
+
+    system("PAUSE");
+    return 0;
+}
+
